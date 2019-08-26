da0594f8570f9c2f22b5b06ac21294856464ce44
Zack Creach <zackcreach@gmail.com>
2019-08-25 23:16:13 -0400

    Initial working commit.

---
+++ genart/sketch.js
@@ -0,0 +1,54 @@
+const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
+
+const settings = {
+  dimensions: [2048, 2048]
+};
+
+const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 20;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push({
+          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          position: [u, v]
+        });
+      }
+    }
+    return points;
+  };
+
+  // random.setSeed(100);
+  const points = createGrid().filter(() => random.value() > 0.5);
+  const margin = 400;
+
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, width, height);
+
+    points.forEach(data => {
+      const { position, radius } = data;
+
+      const [u, v] = position;
+
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, width - margin, v);
+
+      context.beginPath();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.strokeStyle = "black";
+      context.lineWidth = 50;
+      context.fillStyle = "blue";
+      context.fill();
+    });
+  };
+};
+
+canvasSketch(sketch, settings);
