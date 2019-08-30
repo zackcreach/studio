45fd4ffbaf55483a2b3c3992d807d4033d3eb60e
Zack Creach <zackcreach@gmail.com>
2019-08-29 20:37:16 -0400

    [sketch.js] 2019.08.29-20.37.16

--- genart/sketch.js
+++ genart/sketch.js
@@ -3,32 +3,30 @@ const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
 
-// random.setSeed(random.getRandomSeed());
+random.setSeed(random.getRandomSeed());
 
 const settings = {
-  // suffix: random.getSeed(),
+  suffix: random.getSeed(),
   dimensions: [2048, 2048]
 };
-
-random.getSeed(452513);
 
 const sketch = () => {
   const colorCount = random.rangeFloor(1, 6);
   const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
   const createGrid = () => {
     const points = [];
-    const count = 20;
+    const count = 50;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.4;
+        const radius = Math.abs(random.noise2D(u, v)) * 0.1;
 
         points.push({
           color: random.pick(palette),
           radius,
-          rotation: random.noise2D(u, v),
+          rotation: random.noise2D(u, v) * 2,
           position: [u, v]
         });
       }
@@ -41,7 +39,7 @@ const sketch = () => {
   const margin = 300;
 
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
+    context.fillStyle = "black";
     context.fillRect(0, 0, width, height);
 
     points.forEach(data => {
@@ -62,7 +60,8 @@ const sketch = () => {
       context.font = `${radius * width}px "Helvetica"`;
       context.translate(x, y);
       context.rotate(rotation);
-      context.fillText("=", 0, 0);
+      context.scale(3, 0.5);
+      context.fillText("-", 0, 0);
 
       context.restore();
     });
