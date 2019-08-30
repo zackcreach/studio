a8d8a1c5f6ad84506ec1e79e396fdbbf78d14454
Zack Creach <zackcreach@gmail.com>
2019-08-29 20:16:43 -0400

    [sketch.js] 2019.08.29-20.16.43

--- genart/sketch.js
+++ genart/sketch.js
@@ -3,11 +3,18 @@ const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
 
+// random.setSeed(random.getRandomSeed());
+
 const settings = {
+  // suffix: random.getSeed(),
   dimensions: [2048, 2048]
 };
 
+random.getSeed(452513);
+
 const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6);
+  const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
   const createGrid = () => {
     const points = [];
     const count = 20;
@@ -16,8 +23,12 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.4;
+
         points.push({
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
+          color: random.pick(palette),
+          radius,
+          rotation: random.noise2D(u, v),
           position: [u, v]
         });
       }
@@ -25,28 +36,35 @@ const sketch = () => {
     return points;
   };
 
-  // random.setSeed(100);
-  const points = createGrid().filter(() => random.value() > 0.5);
-  const margin = 400;
+  // random.setSeed(1);
+  const points = createGrid().filter(() => random.value() > 0.1);
+  const margin = 300;
 
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
     points.forEach(data => {
-      const { position, radius } = data;
+      const { position, radius, color, rotation } = data;
 
       const [u, v] = position;
 
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, width - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
-      context.strokeStyle = "black";
-      context.lineWidth = 50;
-      context.fillStyle = "blue";
-      context.fill();
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
+      context.fillStyle = color;
+      context.font = `${radius * width}px "Helvetica"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText("=", 0, 0);
+
+      context.restore();
     });
   };
 };
