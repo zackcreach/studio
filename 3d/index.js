27feb7f400cd6d1d5c439fa4b4ae33342b1aff92
Zack Creach <zackcreach@gmail.com>
2019-09-14 17:26:29 -0400

    Now animating!

--- 3d/index.js
+++ 3d/index.js
@@ -1,5 +1,7 @@
 const random = require("canvas-sketch-util/random");
 const palettes = require("nice-color-palettes");
+const eases = require("eases");
+const BezierEasing = require("bezier-easing");
 
 // Ensure ThreeJS is in global scope for the 'examples/'
 global.THREE = require("three");
@@ -10,6 +12,9 @@ require("three/examples/js/controls/OrbitControls");
 const canvasSketch = require("canvas-sketch");
 
 const settings = {
+  dimensions: [512, 512],
+  fps: 24,
+  duration: 4,
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
@@ -38,7 +43,7 @@ const sketch = ({ context }) => {
   for (let index = 0; index < 50; index++) {
     const mesh = new THREE.Mesh(
       new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshBasicMaterial({
+      new THREE.MeshStandardMaterial({
         color: random.pick(palette)
       })
     );
@@ -55,6 +60,14 @@ const sketch = ({ context }) => {
     scene.add(mesh);
   }
 
+  scene.add(new THREE.AmbientLight("hsl(0%, 0%, 40%)"));
+
+  const light = new THREE.DirectionalLight("white", 1);
+  light.position.set(0, 0, 4);
+  scene.add(light);
+
+  const easeFn = BezierEasing(0.67, 0.03, 0.29, 0.99);
+
   // draw each frame
   return {
     // Handle resize events here
@@ -66,6 +79,9 @@ const sketch = ({ context }) => {
 
       // Ortho zoom
       const zoom = 1.5;
+
+      // Correct for Z
+      camera.up.set(0, 0, 1);
 
       // Bounds
       camera.left = -zoom * aspect;
@@ -85,7 +101,9 @@ const sketch = ({ context }) => {
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render({ time }) {
+    render({ playhead }) {
+      const t = Math.sin(playhead * Math.PI);
+      scene.rotation.z = easeFn(t);
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
