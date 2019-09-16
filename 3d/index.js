04eabf1ae3bfb460f9c793ca7bd4b3a74593a18c
Zack Creach <zackcreach@gmail.com>
2019-09-15 22:05:32 -0400

    Progress on homepage background.

--- 3d/index.js
+++ 3d/index.js
@@ -25,8 +25,8 @@ const sketch = ({ context }) => {
   renderer.setClearColor("black", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
+  const camera = new THREE.PerspectiveCamera(30, 1, 0.01, 1000);
+  camera.position.set(2, 100, -4);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -35,20 +35,13 @@ const sketch = ({ context }) => {
   // Setup your scene
   const scene = new THREE.Scene();
 
-  for (let index = -100; index < 100; index++) {
-    const material = new THREE.LineBasicMaterial({ color: "white" });
+  for (let index = -500; index < 500; index++) {
+    const material = new THREE.LineBasicMaterial({
+      color: `hsl(${index * 10}, 100%, 100%)`
+    });
     const geometry = new THREE.Geometry();
-    geometry.vertices.push(new THREE.Vector3(-100, 0, index * 2));
-    geometry.vertices.push(new THREE.Vector3(100, 0, index * 2));
-    const line = new THREE.Line(geometry, material);
-    scene.add(line);
-  }
-
-  for (let index = -100; index < 100; index++) {
-    const material = new THREE.LineBasicMaterial({ color: "white" });
-    const geometry = new THREE.Geometry();
-    geometry.vertices.push(new THREE.Vector3(-100, 0, index * 2));
-    geometry.vertices.push(new THREE.Vector3(100, 0, index * 2));
+    geometry.vertices.push(new THREE.Vector3(-10000, 0, index * 3));
+    geometry.vertices.push(new THREE.Vector3(10000, 0, index * 3));
     const line = new THREE.Line(geometry, material);
     scene.add(line);
   }
@@ -73,6 +66,8 @@ const sketch = ({ context }) => {
     // Update & render your scene here
     render({ time }) {
       controls.update();
+      scene.rotation.x = time / 30;
+      // scene.rotation.z = time / 50;
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
